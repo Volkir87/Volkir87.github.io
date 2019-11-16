ea778172618df7b835e8c241b3c37ae53bd441c1
Kirill V <kirilliuswest@gmail.com>
2019-11-16 17:37:58 -0500

    added basic website

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Volkir87.github.io
+# Responsive-Portfolio
+
+This is an example of a responsive portfolio website, built using html and css. 
+
+Link to the website:
+https://volkir87.github.io/Responsive-Portfolio/
---
+++ config/projects.js
@@ -0,0 +1,12 @@
+var allProjects = [
+    {
+        name: "Weather Dashboard",
+        desc: "The simple app to display weather in different cities around the globe",
+        link: ""
+    },
+    {
+        name: "Nounify",
+        desc: "An app to turn a simple text into a picture book",
+        link: ""
+    }
+];
